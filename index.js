4a1b113282c2456a9124a0dd2c02dfdba2eff5da
danial <trody2001@gmail.com>
2025-04-14 18:10:20 +0300

    change my color

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function firstInteraction() {
     alert("Hey, it's the first interaction with JS!");
 }
-
-
 firstInteraction();
+
+const buttonJS = document.querySelector(".btn-primary");
+
+if (buttonJS) {
+    buttonJS.addEventListener('click', function() {
+        changeColor();
+    });
+}
+
+function changeColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    buttonJS.style.backgroundColor = randomColor;
+}
